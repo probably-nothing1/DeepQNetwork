09329b9d6073c46ba9f40b39831a465faadb170c
gasiortomasz <gasior.tt@gmail.com>
2020-08-25 21:54:12 +0200

    Refactor model

--- src/models/DeepQNetwork.py
+++ src/models/DeepQNetwork.py
@@ -3,54 +3,22 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-# from utils.utils import create_fully_connected_network
-
-# def train_actor(actor, data, optimizer):
-#     optimizer.zero_grad()
-#     observations = data["observations"]
-#     actions = data["actions"]
-#     theta = data["theta"]
-
-#     _, policy = actor(observations)
-#     log_probs = policy.log_prob(actions)
-#     loss = -(theta * log_probs).mean()
-
-#     entropy = policy.entropy().mean()
-
-#     loss.backward()
-#     optimizer.step()
-#     return loss.item(), entropy.item()
-
-
-# @gin.configurable
-# def create_deep_q_network(observation_space, action_space, hidden_sizes):
-#     observation_dim = observation_space.shape[0]
-#     action_dim = action_space.n
-#     return DeepQNetwork(observation_dim, action_dim, hidden_sizes)
-
 
 class DeepQNetwork(nn.Module):
-    def __init__(self, observation_shape, num_of_actions):
+    def __init__(self, observation_shape, action_space):
         super().__init__()
         self.input_channels = observation_shape[0]
+        self.action_space = action_space
         self.conv_net = nn.Sequential(
-            nn.Conv2d(self.input_channels, 64, 3),
+            nn.Conv2d(self.input_channels, 32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3),
-            nn.ReLU(),
-            nn.Conv2d(64, 32, 3),
-            nn.ReLU(),
-            nn.Conv2d(32, 16, 3),
-            nn.ReLU(),
-            nn.Conv2d(16, 8, 3),
+            nn.Conv2d(64, 64, kernel_size=3),
             nn.ReLU(),
         )
         flatten_size = self._compute_conv_out(observation_shape)
-        self.fc_net = nn.Sequential(
-            nn.Linear(flatten_size, 512), nn.ReLU(), nn.Linear(512, 128), nn.ReLU(), nn.Linear(128, num_of_actions),
-        )
+        self.fc_net = nn.Sequential(nn.Linear(flatten_size, 512), nn.ReLU(), nn.Linear(512, self.action_space.n))
 
     def _compute_conv_out(self, observation_shape):
         dummy_input = torch.zeros(1, *observation_shape)
@@ -58,15 +26,27 @@ class DeepQNetwork(nn.Module):
             out = self.conv_net(dummy_input)
         return int(np.prod(out.shape))
 
-    def forward(self, observation):
-        batch_size = observation.size()[0]
-        conv_out = self.conv_net(observation).view(batch_size, -1)
+    def forward(self, observations):
+        if len(observations.shape) == 3:
+            observations.unsqueeze_(0)
+        conv_out = self.conv_net(observations).view(observations.size(0), -1)
         q_values = self.fc_net(conv_out)
         return q_values
 
-    def get_best_action(self, observation):
-        if len(observation.shape) == 3:
-            observation.unsqueeze_(0)
-        with torch.no_grad():
-            q_values = self.forward(observation)
-        return torch.argmax(q_values, dim=1)
+    def get_q_values(self, observations, actions):
+        q_values = self(observations)
+        return q_values.gather(0, actions.unsqueeze(1)).squeeze()
+
+    @torch.no_grad()
+    def get_max_q_values(self, observations):
+        q_values = self(observations)
+        max_q_values, _ = torch.max(q_values, dim=1)
+        return max_q_values
+
+    @torch.no_grad()
+    def sample(self, observations, epsilon):
+        if torch.rand(1).item() < epsilon:
+            return self.action_space.sample()
+
+        q_values = self(observations)
+        return torch.argmax(q_values, dim=1).item()
